7db6294bb949425c7ef66f533e0f4d6656580035
Santosh0710 <santoshthadhani28@gmail.com>
2026-06-03 14:35:35 +0530

    feat: initial Spring Boot project setup with MySQL configuration

---
+++ src/main/java/smarthirebackend/SmarthireBackendApplication.java
@@ -0,0 +1,13 @@
+package smarthirebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthireBackendApplication.class, args);
+	}
+
+}
